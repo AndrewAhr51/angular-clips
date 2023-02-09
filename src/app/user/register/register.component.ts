080c327e682a39f9e9de93ee83d5b4f8b489ac98
ANDREW AHR <andrew.ahr51@gmail.com>
2023-02-09 11:39:55 -0700

    Added Firebase

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-register',
@@ -8,6 +9,12 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
 })
 
 export class RegisterComponent {
+
+  inSubmission = false;
+
+  constructor(private auth: AngularFireAuth) {
+
+  }
 
   name = new FormControl('',[
     Validators.required,
@@ -51,9 +58,35 @@ export class RegisterComponent {
 
   })
 
-  register() {
+  async register() {
+
+    this.inSubmission = true;
+
     this.showAlert = true
+
     this.alertMsg = 'Please wait your account is being created.'
     this.alertColor ='blue'
+
+    const {email, password} = this.registerForm.value
+
+    try {
+      const userCred = await this.auth.createUserWithEmailAndPassword(
+        email as string, password as string)
+
+        console.log(userCred)
+    }
+    catch(e){
+      console.error(e)
+      this.alertMsg = 'An unexpected error occurred. Please try again later'
+      this.alertColor ='red'
+      this.inSubmission = false;
+      return
+    }
+
+    this.alertMsg = 'Success! Your account has been created.'
+    this.alertColor ='green'
+
+    this.inSubmission = false;
+
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD10xAi7nCYJvHg4lwb6Jd0Gd_VcUDeuxA",
+    authDomain: "clips-d20ce.firebaseapp.com",
+    projectId: "clips-d20ce",
+    storageBucket: "clips-d20ce.appspot.com",
+    messagingSenderId: "902615826713",
+    appId: "1:902615826713:web:ac9dfb3264148df86c4588"
+  }
+};
