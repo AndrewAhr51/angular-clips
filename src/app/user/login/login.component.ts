f95dae80ea60f082dabddbf1d5cb0860a55576ea
ANDREW AHR <andrew.ahr51@gmail.com>
2023-02-11 05:44:32 -0700

    Login form authentication

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import  IUser from 'src/app/models/user.model'
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,7 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 
-export class LoginComponent{
-
-  showAlert=false
-  alertMsg= 'Please wait! Your are being logged into the application.'
-  alertColor ='green'
+export class LoginComponent implements OnInit {
 
   email=  new FormControl('',[
     Validators.required,
@@ -24,15 +22,42 @@ export class LoginComponent{
 
   loginForm = new FormGroup({
     email:  this.email,
-    password: this.password
+    password: this.password,
+
   })
 
-  login() {
-    console.log(this.loginForm.value)
-    this.showAlert = true
-    this.alertMsg= 'Please wait! Your are being logged into the application.'
+  ngOnInit(): void {}
+
+  showAlert=false
+  alertMsg= 'Please wait! Your are being logged into the application.'
+  alertColor ='blue'
+  inSubmission = false;
+
+  constructor(
+    private auth: AuthService
+    ) { }
+
+  async login() {
+    try {
+      this.inSubmission = true;
+      this.showAlert = true
+      this.alertMsg= 'Please wait! Your are being logged into the application.'
+      this.alertColor ='blue'
+
+      await this.auth.loginUser(this.loginForm.value as IUser)
+    }
+    catch(e) {
+      this.alertMsg = 'An unexpected error occurred. Please try again later'
+      this.alertColor ='red'
+      this.inSubmission = false;
+      console.log(e)
+      return
+    }
+
+    this.alertMsg = 'Success! Your are now logged in..'
     this.alertColor ='green'
+
+    this.inSubmission = false;
   }
 
-  get formInfo() { return this.loginForm.controls; }
 }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AuthService } from 'src/app/services/auth.service';
+import  IUser from 'src/app/models/user.model'
 
 @Component({
   selector: 'app-register',
@@ -12,9 +12,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 export class RegisterComponent {
 
   constructor(
-    private auth: AngularFireAuth,
-    private db: AngularFirestore
-
+   private auth: AuthService
   ){}
 
   inSubmission = false;
@@ -27,7 +25,7 @@ export class RegisterComponent {
     Validators.required,
     Validators.email,
   ])
-  age= new FormControl('',[
+  age= new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
     Validators.max(120)
@@ -63,25 +61,13 @@ export class RegisterComponent {
 
   async register() {
 
-    this.inSubmission = true;
-
     this.showAlert = true
-
     this.alertMsg = 'Please wait your account is being created.'
     this.alertColor ='blue'
-
-    const {email, password} = this.registerForm.value
+    this.inSubmission = true;
 
     try {
-      const userCred = await this.auth.createUserWithEmailAndPassword(
-        email as string, password as string)
-
-       await this.db.collection('users').add({
-        name:this.name.value,
-        email: this.email.value,
-        age: this.age.value,
-        phoneNumber: this.phoneNumber.value
-       })
+      await this.auth.createUser(this.registerForm.value as IUser)
     }
     catch(e){
       console.error(e)
